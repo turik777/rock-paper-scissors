eede88c24ff1ec2a729a3c45516de9c46203937f
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-04 23:25:11 +0300

    Add function to play 1 round of Rock Paper Scissors

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = (prompt("Please select rock, paper or scissors.")).toLowerCase();
+    computerSelection = (getComputerChoice()).toLowerCase();
+    if ((playerSelection === "rock" && computerSelection === "paper") || 
+        (playerSelection === "paper" && computerSelection === "scissors") || 
+        (playerSelection === "scissors" && computerSelection === "rock")) {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") || 
+               (playerSelection === "paper" && computerSelection === "rock") || 
+               (playerSelection === "scissors" && computerSelection === "paper")) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return "Tie!"
+    }
+}
