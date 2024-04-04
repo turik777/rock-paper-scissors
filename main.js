eb8b2b48fa6ae74550d7316998a3d687f0b3540a
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-04 22:54:36 +0300

    Add function that selects computer choice

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log(1);
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
+        return "Rock";
+    } else if (computerChoice === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
