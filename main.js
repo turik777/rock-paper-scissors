3fa7dce679e810ed3cffe0e9c9d1a0a742c5f04c
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-07 18:13:27 +0300

    Refactor code

--- main.js
+++ main.js
@@ -13,27 +13,35 @@ let playerScore = 0;
 let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-    let result = "";
     computerSelection = getComputerChoice();
     if ((playerSelection === "rock" && computerSelection === "paper") || 
         (playerSelection === "paper" && computerSelection === "scissors") || 
         (playerSelection === "scissors" && computerSelection === "rock")) {
         computerScore++;
-        result = `You Lose! ${computerSelection} beats ${playerSelection}. Score: ${playerScore}:${computerScore}`;
+        results.textContent = `You Lose! ${computerSelection} beats ${playerSelection}. Score: ${playerScore}:${computerScore}`;
     } else if ((playerSelection === "rock" && computerSelection === "scissors") || 
                (playerSelection === "paper" && computerSelection === "rock") || 
                (playerSelection === "scissors" && computerSelection === "paper")) {
         playerScore++;
-        result = `You Win! ${playerSelection} beats ${computerSelection}. Score: ${playerScore}:${computerScore}`;
+        results.textContent = `You Win! ${playerSelection} beats ${computerSelection}. Score: ${playerScore}:${computerScore}`;
     } else {
-        result = `Tie! Score: ${playerScore}:${computerScore}`;
+        results.textContent = `Tie! Score: ${playerScore}:${computerScore}`;
     }
-    console.log(result);
+    if (playerScore === 5) {
+        results.textContent = `Player Win! ${playerScore}:${computerScore}`;
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5){
+        results.textContent = `Computer Win! ${playerScore}:${computerScore}`;
+        playerScore = 0;
+        computerScore = 0;
+    }
 }
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
+const results = document.querySelector("#results");
 
 rock.addEventListener("click", () => playRound("rock"));
 paper.addEventListener("click", () => playRound("paper"));
